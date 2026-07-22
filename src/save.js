878d08a451c75a29d990ea0ce8d6dd87c485565a
Fehdi69 <144682055+Fehdi69@users.noreply.github.com>
2026-07-22 14:56:35 +0000

    Refactor Save component to implement accordion functionality for intro section

--- src/save.js
+++ src/save.js
@@ -5,13 +5,21 @@ export default function Save({ attributes }) {
 
   return (
     <div {...useBlockProps.save()} className="db-documentation">
-      <RichText.Content tagName="h1" value={title} />
-
-      {intro.map((block, index) => (
-        <p key={index}>{block.text}</p>
-      ))}
-
       <div className="db-accordion">
+        <div className="db-item">
+          <button className="db-title" type="button">
+            {title || "Introduction"}
+
+            <span className="db-icon">+</span>
+          </button>
+
+          <div className="db-content">
+            {intro.map((block, index) => (
+              <p key={index}>{block.text}</p>
+            ))}
+          </div>
+        </div>
+
         {sections.map((section, index) => (
           <div className="db-item" key={index}>
             <button className="db-title" type="button">
